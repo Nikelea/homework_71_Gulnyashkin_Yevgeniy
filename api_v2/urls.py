489f7f283b45a6a16927f26f6c9ddcaebb6ea33f
Гульняшкин Евгений <ducat.ea@gmail.com>
2023-04-20 15:22:42 +0600

    Указал пути и проверил на работоспособность CRUD публикаций через АПИ

---
+++ api_v2/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework import routers
+from api_v2 import views
+from api_v2.views import LogoutView, PubViewSet, PubListView, PubDetailView
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'publications', views.PubViewSet)
+
+app_name = 'api_v2'
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', obtain_auth_token, name='api_token_auth'),
+    path('like/<int:pk>', PubViewSet.as_view({'get' : 'like_it'}), name='like_it'),
+    path('logout/', LogoutView.as_view(), name='api_token_delete'),
+]
+
+# urlpatterns +=  [
+#     path('pubs', PubListView.as_view(), name='list_pubs_api'),
+#     path('pubs/<int:pk>', PubDetailView.as_view(), name='detail_pubs_api'),
+# ]
